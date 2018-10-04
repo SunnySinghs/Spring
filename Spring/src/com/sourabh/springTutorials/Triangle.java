fb6e908abe5c9fcc6402a3e4bb0090d87dee632f
SunnySinghs <sourabh.singh000@gmail.com>
2018-10-05 01:01:59 +0530

    Commit 3

--- Spring/src/com/sourabh/springTutorials/Triangle.java
+++ Spring/src/com/sourabh/springTutorials/Triangle.java
@@ -3,15 +3,31 @@ package com.sourabh.springTutorials;
 public class Triangle {
 	private String type;
 	private int height;
+	private int width;
+	private String type1;
 	private Points pointA;
 	private Points pointB;
 	private Points pointC;
+	private Points pointD;
 	
-	public Triangle(String type, Points pointA) {
+	public Triangle(int width) {
+		this.width = width;
+	}
+	public Triangle(String type1) {
+		this.type1 = type1;
+	}
+	public Triangle(String type, Points pointA, Points pointD) {
 		this.type = type;
 		this.pointA = pointA;
+		this.pointD = pointD;
 	}
 	
+	public String getType1() {
+		return type1;
+	}
+	public void setType1(String type1) {
+		this.type1 = type1;
+	}
 	public Points getPointA() {
 		return pointA;
 	}
@@ -36,10 +52,22 @@ public class Triangle {
 		this.pointC = pointC;
 	}
 
+	public Points getPointD() {
+		return pointD;
+	}
+	public void setPointD(Points pointD) {
+		this.pointD = pointD;
+	}
 	public int getHeight() {
 		return height;
 	}
 
+	public int getWidth() {
+		return width;
+	}
+	public void setWidth(int width) {
+		this.width = width;
+	}
 	public void setHeight(int height) {
 		this.height = height;
 	}
@@ -53,9 +81,10 @@ public class Triangle {
 	}
 	
 	public void draw() {
-		System.out.println("This is a "+getType()+" Triangle of height "+getHeight());
+		System.out.println("This is a "+getType()+" Triangle of height "+getHeight() + " and width "+getWidth());
 		System.out.println("Point A's x is "+getPointA().getX()+ " and y is "+getPointA().getY());
-		System.out.println("Point B's x is "+getPointA().getX()+ " and y is "+getPointB().getY());
-		System.out.println("Point A's x is "+getPointC().getX()+ " and y is "+getPointC().getY());
+		System.out.println("Point B's x is "+getPointB().getX()+ " and y is "+getPointB().getY());
+		System.out.println("Point C's x is "+getPointC().getX()+ " and y is "+getPointC().getY());
+		System.out.println("Point D's x is "+getPointD().getX()+ " and y is "+getPointD().getY());
 	}
 }
