c0dd1a19a7696e4b3afe73d0bd97ceb5770d782d
sourabh.singh <sourabh.singh@franconnet.com>
2018-10-04 19:33:44 +0530

    Commit 2

--- Spring/src/com/sourabh/springTutorials/Triangle.java
+++ Spring/src/com/sourabh/springTutorials/Triangle.java
@@ -1,8 +1,61 @@
 package com.sourabh.springTutorials;
 
 public class Triangle {
+	private String type;
+	private int height;
+	private Points pointA;
+	private Points pointB;
+	private Points pointC;
+	
+	public Triangle(String type, Points pointA) {
+		this.type = type;
+		this.pointA = pointA;
+	}
+	
+	public Points getPointA() {
+		return pointA;
+	}
 
+	public void setPointA(Points pointA) {
+		this.pointA = pointA;
+	}
+
+	public Points getPointB() {
+		return pointB;
+	}
+
+	public void setPointB(Points pointB) {
+		this.pointB = pointB;
+	}
+
+	public Points getPointC() {
+		return pointC;
+	}
+
+	public void setPointC(Points pointC) {
+		this.pointC = pointC;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+	
 	public void draw() {
-		System.out.println("This is a Triangle");
+		System.out.println("This is a "+getType()+" Triangle of height "+getHeight());
+		System.out.println("Point A's x is "+getPointA().getX()+ " and y is "+getPointA().getY());
+		System.out.println("Point B's x is "+getPointA().getX()+ " and y is "+getPointB().getY());
+		System.out.println("Point A's x is "+getPointC().getX()+ " and y is "+getPointC().getY());
 	}
 }
